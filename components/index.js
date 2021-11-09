8a726302e00bdfafed3532fc2b09323220b01d93
unknown <matt.mcquain@gmail.com>
2021-11-08 22:32:13 -0800

    @47min into video. Header and styling working

---
+++ components/index.js
@@ -0,0 +1,6 @@
+// This index.js allows us to more easily export/import our components
+export { default as PostCard } from './PostCard';
+export { default as Categories } from './Categories';
+export { default as PostWidget } from './PostWidget';
+export { default as Header } from './Header';
+export { default as Layout } from './Layout';
