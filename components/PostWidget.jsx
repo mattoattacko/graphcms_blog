791233347fd877c82c66de8c4226dbfc45c552f9
unknown <matt.mcquain@gmail.com>
2021-11-09 21:58:08 -0800

    PostWidget and Layout complete. @1:29 in video

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  // We want to know if we are seeing the PostWidget on the homepage, or in a specific articles page because...
+  // On the homepage we see the most Recent Posts 
+  // But if we go to the actual articles page, we should see Related Posts
+  // To know the difference we can check the slug, which we pass in to the PostWidget component
+  // The slug is the path of the specific article 
+
+  // We wanted to get similar or related posts if we have a slug (eg: if we are viewing a certain article at the moment)
+  // If we are not, then we get recent posts instead
+  useEffect(() => {
+    if(slug) {
+      getSimilarPosts(categories, slug)
+        .then((result) => setRelatedPosts(result))
+    } else {
+      getRecentPosts()
+      .then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+
+  console.log(relatedPosts)
+
   return (
-    <div>
-      PostWidget
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className='flex items-center w-full mb-4'>
+          <div className='w-16 flex-none'>
+            <img 
+              src={post.featuredImage.url}
+              alt={post.title}
+              height='60px'
+              width='60px'
+              className='align-middle rounded-full'
+            />
+          </div>
+          <div className='flex-grow ml-4'>
+            <p className='text-gray-500 font-xs'>
+              {moment(post.date).format('MMM DD, YYYY')}
+            </p>
+            <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
--- services/index.js
+++ services/index.js
@@ -1,1 +1,89 @@
 import { request, gql } from 'graphql-request';
+
+// Creates our graphql API by using the endpoint from our env
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+// const graphqlAPI = 'https://api-us-west-2.graphcms.com/v2/ckvn962nt1wb701z280an9cyw/master';
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `
+
+  // This fetches the posts
+  const result = await request(graphqlAPI, query);
+
+  // These are the posts
+  return result.postsConnection.edges;
+};
+
+// Gets the newest posts as they are created
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_DESC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
+
+// slug_not = dont display the current article but display other articles that display some of the categories related
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {  
+      posts(
+        where: { slug_not: $slug, AND: { categories_some: { slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
